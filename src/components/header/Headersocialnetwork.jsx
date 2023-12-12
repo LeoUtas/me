5d3181031a4018e6e455a65f027052c60b154a1b
LeoUTas <hoang.nguyen0@utas.edu.au>
2023-12-11 21:57:10 -0330

    make Github button for HeaderSocialNetwork_icons

--- src/components/header/Headersocialnetwork.jsx
+++ src/components/header/Headersocialnetwork.jsx
@@ -1,18 +1,27 @@
-import React from "react";
-import { AiFillLinkedin } from "react-icons/ai";
+import React from 'react';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
 
 function Headersocialnetwork() {
-    return (
-        <div className="Headersocialnetwork_icons">
-            <a
-                href="https://www.linkedin.com"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <AiFillLinkedin className="icon_items" />
-            </a>
-        </div>
-    );
+  return (
+    <div className="Headersocialnetwork_icons">
+      <a
+        href="https://www.linkedin.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <AiFillLinkedin className="icon_items" />
+      </a>
+
+      <a
+        href="https://github.com/LeoUtas"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <AiFillGithub className="icon_items" />
+      </a>
+    </div>
+  );
 }
 
 export default Headersocialnetwork;
