56e236414db9dfcc1bb085059a0103a3dd78241a
LeoUTas <hoang.nguyen0@utas.edu.au>
2024-01-02 12:43:59 -0330

    add filer projects for category

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,22 +1,89 @@
 import './projects.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import projectData0 from '../../assets/data/project_data.json';
 import { FaGithub } from 'react-icons/fa';
 
+const CATEGORIES = [
+  { name: 'Highlights' },
+  { name: 'Machine Learning' },
+  { name: 'Deep Learning' },
+  { name: 'Computer Vision' },
+  { name: 'Data Visualization' },
+  { name: 'From Scratch' },
+  { name: 'Data Analytics' },
+  { name: 'Natural Language Processing' },
+  { name: 'Web Development' },
+  { name: 'Tiny Repos' },
+];
+
+// filter the category
+function CategoryFilter({ setCurrentCategory, availableCategories }) {
+  return (
+    <div className="categories">
+      <ul>
+        {CATEGORIES.filter(category_arg =>
+          availableCategories.includes(category_arg.name)
+        ).map(category_arg => (
+          <li key={category_arg.name}>
+            <button
+              className="btn category-btn"
+              onClick={() => setCurrentCategory(category_arg.name)}
+            >
+              {category_arg.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 function Projects() {
   const [projects, setProjects] = useState([]);
+  const [currentCategory, setCurrentCategory] = useState('Highlights');
+
+  // Calculate availableCategories
+  const availableCategories = useMemo(() => {
+    return projectData0.reduce((accumulator, project) => {
+      const projectCategories = Array.isArray(project.category)
+        ? project.category
+        : [project.category];
+      projectCategories.forEach(category => {
+        if (!accumulator.includes(category)) {
+          accumulator.push(category);
+        }
+      });
+      return accumulator;
+    }, []);
+  }, []); // Empty dependency array ensures this only runs once
 
   useEffect(() => {
     const projectData = projectData0.sort((a, b) => b.id - a.id);
     setProjects(projectData);
   }, []);
 
+  const filteredProjects = useMemo(
+    () =>
+      projects.filter(project => {
+        if (Array.isArray(project.category)) {
+          return project.category.includes(currentCategory);
+        }
+        return project.category === currentCategory;
+      }),
+    [projects, currentCategory]
+  );
+
   return (
     <section id="projects">
-      <h2 className="section_title">Repository highlights</h2>
+      <h2 className="section_title">My Repositories</h2>
+
+      <CategoryFilter
+        setCurrentCategory={setCurrentCategory}
+        availableCategories={availableCategories}
+      />
 
       <div className="container project_container">
-        {projects.map(
+        {filteredProjects.map(
           ({
             id,
             featured_image,
@@ -24,52 +91,43 @@ function Projects() {
             project_summary,
             github,
             demo,
-            featured_work,
-          }) =>
-            featured_work && (
-              <article key={id} className="project_item">
-                <div className="project_item-image">
-                  <img
-                    src={process.env.PUBLIC_URL + featured_image}
-                    alt={project_title}
-                  />
-                </div>
-                <h3>{project_title}</h3>
-                <p>{project_summary}</p>
-                <div className="project_btn">
+            technical_tools,
+          }) => (
+            <article key={id} className="project_item">
+              <div className="project_item-image">
+                <img
+                  src={process.env.PUBLIC_URL + featured_image}
+                  alt={project_title}
+                />
+              </div>
+              <h3>{project_title}</h3>
+              <p>{project_summary}</p>
+              {/* <p className="">{technical_tools}</p> */}
+              <div className="project_btn">
+                <a
+                  href={github}
+                  className="btn"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+                {/* Conditional rendering for the demo button */}
+                {demo && (
                   <a
-                    href={github}
-                    className="btn"
+                    href={demo}
+                    className="btn btn-primary"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    Github
+                    Demo
                   </a>
-                  {/* Conditional rendering for the demo button */}
-                  {demo && (
-                    <a
-                      href={demo}
-                      className="btn btn-primary"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Demo
-                    </a>
-                  )}
-                </div>
-              </article>
-            )
+                )}
+              </div>
+            </article>
+          )
         )}
       </div>
-
-      <h2 className="section_title for_more">
-        For more interesting repositories, welcome to visit my {''}
-        <span>
-          <a href="https://github.com/LeoUtas" target="_blank" rel="noreferrer">
-            Github <FaGithub />
-          </a>
-        </span>
-      </h2>
     </section>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-// 
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-
-ReactDOM.render(<App/>, document.querySelector('#root'));
+// Create a root.
+const container = document.querySelector('#root');
+const root = createRoot(container);
+root.render(<App tab="home" />);
