0cfbf7d27b87547e718e82b3f56238bcf7a377b8
LeoUTas <hoang.nguyen0@utas.edu.au>
2024-05-13 23:44:38 -0230

    make a new version

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,29 +2,14 @@ import React from 'react';
 import './header.css';
 import { Canvas } from '@react-three/fiber';
 import Particles from './particlesnoise';
-// import TypingEffect from "./typingEffect";
 import Headersocialnetwork from './Headersocialnetwork';
-import { FaGithub } from 'react-icons/fa';
-import { FaLinkedin } from 'react-icons/fa';
 
 function Header() {
   return (
     <header>
       <div className="container header_container">
-        <h1>Good day, I'm Hoang</h1>
-        <h2 className="header_h3_icons">
-          <a href="https://github.com/LeoUtas" target="_blank" rel="noreferrer">
-            <FaGithub />{' '}
-          </a>
+        <h1>GOOD DAY, I'M HOANG</h1>
 
-          <a
-            href="https://www.linkedin.com/in/hoangng84/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaLinkedin />
-          </a>
-        </h2>
         <h3>
           I study and solve problems using technical tools (i.e., Figma for UI
           design, React for the frontend, Python for the backend, model
@@ -34,7 +19,7 @@ function Header() {
 
         <Canvas
           style={{
-            position: 'absolute',
+            position: 'flex',
             width: '100%',
             height: '100%',
           }}
