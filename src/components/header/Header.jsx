ae77d885985d4b3a1ff42f5b2fd34351e8958d86
Hoang Nguyen The <hoangnguyenthe@hoangs-mbp.home>
2024-05-15 11:35:28 -0230

    commit main

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
         <h3>
           I'm actively looking for a developer position. I have a quantitative
           background by train and have built multiple full-stack applications,
-          including IOS, web development, and computer vision modelling.
+          including IOS, web development, and computer vision modelling
         </h3>
 
         <Canvas className="canvas">
