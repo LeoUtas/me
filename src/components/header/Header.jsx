7ffb5726c00f22b8af115d8375d9e795f2f598c6
Hoang Nguyen The <hoangnguyenthe@hoangs-mbp.home>
2024-05-15 02:10:04 -0230

    revise header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './header.css';
 import { Canvas } from '@react-three/fiber';
@@ -29,57 +28,3 @@ function Header() {
 }
 
 export default Header;
-=======
-import React from 'react';
-import './header.css';
-import { Canvas } from '@react-three/fiber';
-import Particles from './particlesnoise';
-// import TypingEffect from "./typingEffect";
-import Headersocialnetwork from './Headersocialnetwork';
-import { FaGithub } from 'react-icons/fa';
-import { FaLinkedin } from 'react-icons/fa';
-
-function Header() {
-  return (
-    <header>
-      <div className="container header_container">
-        <h1>Good day, I'm Hoang</h1>
-        <h2 className="header_h3_icons">
-          <a href="https://github.com/LeoUtas" target="_blank" rel="noreferrer">
-            <FaGithub />{' '}
-          </a>
-
-          <a
-            href="https://www.linkedin.com/in/hoangng84/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaLinkedin />
-          </a>
-        </h2>
-        <h3>
-          I study and solve problems using technical tools (i.e., Figma for UI
-          design, React for the frontend, Python for the backend, model
-          training, and cloud services for the database). I am always learning
-          to be a better problem solver.
-        </h3>
-
-        <Canvas
-          style={{
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-          }}
-        >
-          <Particles count={8000} />
-        </Canvas>
-        <div className="header_items">
-          <Headersocialnetwork className="header_item" />
-        </div>
-      </div>
-    </header>
-  );
-}
-
-export default Header;
->>>>>>> fc5bda79ab38a461aff32927302b2241a0378c18
