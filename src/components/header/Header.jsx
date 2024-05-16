0d31b2c8b2e6f55b1984a96ee3742d72cd5b9b81
Hoang Nguyen The <hoangnguyenthe@hoangs-mbp.home>
2024-05-15 21:56:55 -0230

    revise after merge main

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './header.css';
 import { Canvas } from '@react-three/fiber';
@@ -29,35 +28,3 @@ function Header() {
 }
 
 export default Header;
-=======
-import React from 'react';
-import './header.css';
-import { Canvas } from '@react-three/fiber';
-import Particles from './particlesnoise';
-import Headersocialnetwork from './Headersocialnetwork';
-
-function Header() {
-  return (
-    <header>
-      <div className="container header_container">
-        <h1>GOOD DAY, I'M HOANG</h1>
-
-        <h3>
-          I'm actively looking for a developer position. I have a quantitative
-          background and have built multiple full-stack applications, including
-          IOS, web development, computer vision modelling
-        </h3>
-
-        <Canvas className="canvas">
-          <Particles count={8000} />
-        </Canvas>
-        <div className="header_items">
-          <Headersocialnetwork className="header_item" />
-        </div>
-      </div>
-    </header>
-  );
-}
-
-export default Header;
->>>>>>> main
