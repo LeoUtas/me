2ba91fd3cde12615d67e5570d4864f561ba34a65
Hoang Nguyen The <hoangnguyenthe@hoangs-mbp.home>
2024-05-17 12:12:56 -0230

    revise the heading text

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,9 +11,9 @@ function Header() {
         <h1>GOOD DAY, I'M HOANG</h1>
 
         <h3>
-          I'm actively looking for an entry-level position as a full-stack
-          developer. The vision for my future development is to dive deeper into
-          backend engineering.
+          I'm actively looking for a developer position. I have built multiple
+          full-stack applications and some computer vision modelling. The vision
+          for my future development is to dive deeper into backend engineering.
         </h3>
 
         <Canvas className="canvas">
